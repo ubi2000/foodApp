1d918bdbada6ba5784928e68051ece091ea75723
obaid <obaidlone@gildwaretechnologies.com>
2023-11-13 00:57:01 +0530

    setting backend

--- foodApp/backend/db.js
+++ foodApp/backend/db.js
@@ -1,14 +1,19 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const mongoURI='mongodb+srv://obaidnabi1991:leo1991leo@clusterfoodme.agguzyk.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI =
+  "mongodb+srv://obaidnabi1991:leo1991leo@clusterfoodme.agguzyk.mongodb.net/foodMe?retryWrites=true&w=majority";
 
-
-const dbConnection =  () => {
-    mongoose.connect(mongoURI)
-    .then( ()=>
-       console.log("Connected to mongo Successful")
-   )
+const connectToMongo = async () => {
+ try { 
+    await mongoose.connect(mongoURI) 
+    console.log("Connected to mongo Successful");
+    const fetchedData = await mongoose.connection.db.collection("foodItems");
+    const data= await fetchedData.find({}).toArray()
+}
+catch(error){
+    console.log(error)
 }
 
+};
 
-module.exports=dbConnection
+module.exports = connectToMongo;
--- foodApp/backend/index.js
+++ foodApp/backend/index.js
@@ -2,12 +2,18 @@ const express=require("express")
 const mongoose=require("mongoose")
 const dbConnection=require("./db")
 const app =express()
-
+app.use(express.json())
 const port=5000
 dbConnection()
+const userRouter=require("./Routes/User")
+
+
 app.get("/",(req,res)=>{
     res.send("Hello")
 })
+app.use("/user",userRouter)
+
+
 app.listen(port,()=>{
     console.log("server runing")
 })
